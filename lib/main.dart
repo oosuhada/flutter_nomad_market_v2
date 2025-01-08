43d6d6b8e66f04832d1ee0ae2036891d7a13facd
oosuhada <woosu91@gmail.com>
2025-01-08 20:24:41 +0900

    feat(app): implement main screen logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,31 @@
-// main.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_nomad_market_v2/screens/chat_screen.dart';
-import 'package:flutter_nomad_market_v2/screens/community_screen.dart';
-import 'package:flutter_nomad_market_v2/screens/culture_guide_screen.dart';
-import 'package:flutter_nomad_market_v2/screens/home_screen.dart';
-import 'package:flutter_nomad_market_v2/screens/product_detail_screen.dart';
-import 'package:flutter_nomad_market_v2/screens/safety_center_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
+
+import 'data/demo_marketplace_data.dart';
+import 'screens/chat_screen.dart';
+import 'screens/culture_guide_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/product_detail_screen.dart';
+import 'screens/safety_center_screen.dart';
+import 'theme/nomad_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NomadMarketApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NomadMarketApp extends StatelessWidget {
+  const NomadMarketApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Nomad Market',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.grey[50],
-        textTheme: GoogleFonts.notoSansTextTheme(),
-      ),
-      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      theme: NomadTheme.light(),
+      home: const HomeScreen(),
       routes: {
-        '/': (context) => const HomeScreen(),
-        '/product': (context) => const ProductDetailScreen(),
+        '/product': (context) =>
+            ProductDetailScreen(item: demoMarketplaceItems.first),
         '/chat': (context) => const ChatScreen(),
-        '/community': (context) => const CommunityScreen(),
         '/culture-guide': (context) => const CultureGuideScreen(),
         '/safety-center': (context) => const SafetyCenterScreen(),
       },
